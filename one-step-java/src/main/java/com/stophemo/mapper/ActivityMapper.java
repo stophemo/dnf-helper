6322f3e808477b25a8057437b0abbb427f4c2719
huojie <stepho@foxmail.com>
2023-07-03 17:19:50 +0800

    fill

---
+++ one-step-java/src/main/java/com/stophemo/mapper/ActivityMapper.java
@@ -0,0 +1,7 @@
+package com.stophemo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.stophemo.entity.Activity;
+
+public interface ActivityMapper extends BaseMapper<Activity> {
+}
