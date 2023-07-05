412794d89ab649af41c49e4f6a649ae833f69ecd
huojie <stepho@foxmail.com>
2023-07-05 17:30:28 +0800

    add some html

--- one-step-client/src/main.js
+++ one-step-client/src/main.js
@@ -1,18 +1,102 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu,ipcMain, nativeTheme } = require('electron')
+const path = require('path')
 
-const createWindow = () => {
+function createWindow () {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     })
 
     win.loadFile('./src/index.html')
 }
 
+const template = [
+    {
+        label: '&File',
+        submenu: [
+            { role: 'quit' }
+        ]
+    },
+    {
+        label: '&Edit',
+        submenu: [
+            { role: 'undo' },
+            { role: 'redo' },
+            { type: 'separator' },
+            { role: 'cut' },
+            { role: 'copy' },
+            { role: 'paste' },
+            { role: 'pasteAndMatchStyle' },
+            { role: 'delete' },
+            { role: 'selectAll' },
+            { type: 'separator' },
+            {
+                label: 'Speech',
+                submenu: [
+                    { role: 'startSpeaking' },
+                    { role: 'stopSpeaking' }
+                ]
+            }
+        ]
+    },
+    {
+        label: '&View',
+        submenu: [
+            { role: 'reload' },
+            { role: 'forceReload' },
+            { type: 'separator' },
+            { role: 'resetZoom' },
+            { role: 'zoomIn' },
+            { role: 'zoomOut' },
+            { type: 'separator' },
+            { role: 'togglefullscreen' },
+            { role: 'toggleDevTools' , accelerator: 'F12'},
+        ]
+    },
+    {
+        label: '&Window',
+        submenu: [
+            { role: 'minimize' },
+            { role: 'zoom' },
+            { type: 'separator' },
+            { role: 'front' },
+            { type: 'separator' },
+            { role: 'window' },
+            { role: 'close' }
+        ]
+    },
+    {
+        role: '&help',
+        submenu: [
+            {
+                label: 'Learn More',
+                click: async () => {
+                    const { shell } = require('electron')
+                    await shell.openExternal('https://electronjs.org')
+                }
+            }
+        ]
+    }
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
+
 app.whenReady().then(() => {
     createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
 })
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
